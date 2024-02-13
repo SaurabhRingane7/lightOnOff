78d6ce91c66e70a7f837750dc317493ab8e02412
SaurabhRingane7 <154659768+SaurabhRingane7@users.noreply.github.com>
2024-02-13 16:21:53 +0530

    Add files via upload

---
+++ lightbul.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+var m=document.getElementById("bulIcon")
+console.log(m)
+
+
+function on(){
+	document.getElementById("bulIcon").style.textShadow="0px 10px 61px orange"
+	 document.getElementById("bulIcon").style.color="yellow"
+}
+
+
+function off(){
+	document.getElementById("bulIcon").style.textShadow="0px 0px 0px white"
+	 document.getElementById("bulIcon").style.color="white"
+}
